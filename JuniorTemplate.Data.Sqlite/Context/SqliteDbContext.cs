13bbb27b192826e4506f7254720e210b69ea9bb0
jurilents <keeeneeek@gmail.com>
2022-07-25 23:05:18 +0300

    Add Database.EnsureCreated for hot preview

--- JuniorTemplate.Data.Sqlite/Context/SqliteDbContext.cs
+++ JuniorTemplate.Data.Sqlite/Context/SqliteDbContext.cs
@@ -6,12 +6,16 @@ namespace JuniorTemplate.Data.Context;
 
 public class SqliteDbContext : DbContext, IDatabaseContext
 {
-	public SqliteDbContext(DbContextOptions options) : base(options) { }
+    public SqliteDbContext(DbContextOptions options) : base(options)
+    {
+        // TODO: Remove for your project or be the most careful with this!!! :)
+        Database.EnsureCreated();
+    }
 
 
-	protected override void OnModelCreating(ModelBuilder builder)
-	{
-		builder.ApplyConfigurations();
-		builder.ApplyDataSeeders();
-	}
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurations();
+        builder.ApplyDataSeeders();
+    }
 }
